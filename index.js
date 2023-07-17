7ea11ad05e69183b3d1f5e3e0b893856ef226ca8
Daniel Rocha <danielldante@gmail.com>
2023-07-16 21:29:42 -0300

    Variables and console

---
+++ index.js
@@ -0,0 +1,16 @@
+var carro = "Chevrolet";
+console.log(carro);
+
+const pi = 3.14;
+let resultado = pi * 100;
+console.log(resultado);
+
+let primeiroAno = 100;
+let segundoAno = 100;
+let resultado2 = primeiroAno + segundoAno;
+console.log(resultado2);
+
+let nome = "Daniel";
+let sobrenome = "Rocha";
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
